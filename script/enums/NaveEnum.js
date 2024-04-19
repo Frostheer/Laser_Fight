99eec00d460a1c664d6c66e3c51d66f849189161
Claudio Espinoza <claudioyugen@gmail.com>
2024-04-19 17:47:18 -0400

    Feat: Crear enumarador de nave

---
+++ script/enums/NaveEnum.js
@@ -0,0 +1,17 @@
+
+export class NaveEnum {
+
+    static POSICION_INICIAL_X_NAV_1 = 120;
+    static POSICION_INICIAL_X_NAV_2 = 880;
+    static POSICION_INICIAL_Y_NAV = 300;
+
+    static COLOR_NAV_1 = "blue";
+    static COLOR_NAV_2 = "red";
+
+    static ANCHO_NAV = 20;
+    static LARGO_NAV = 30;
+
+    static NOMBRE_NAV_1 = "Nave 1"
+    static NOMBRE_NAV_2 = "Nave 2"
+
+}
