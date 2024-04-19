9cb9135d30375d6f86472da4d02b4508c880f9fd
Esteban Candia <E.candxa@gmail.com>
2024-04-19 16:47:58 -0400

    Update game

--- script/script_game.js
+++ script/script_game.js
@@ -1,11 +1,12 @@
-import {Nave} from "./Objetos/Nave";
-
+import {Nave} from "./Objetos/Nave.js";
 
 // Referencia inicial al canvas y su respectivo contexto 2d
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-const nave = Nave(0,0,'green',"Nave 1",10,10)
+var nave = new Nave(0,0,'green', "nave 1",10,10)
+
+
 
 var x = canvas.width / 2;
 var y = canvas.height - 30;
@@ -15,7 +16,27 @@ var dy = -2;
 
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
+
+
+function drawPlayer(){
+
+    ctx.beginPath()
+    ctx.rect(nave.posicion_X,nave.posicion_Y,nave.ancho,nave.largo)
+    ctx.fillStyle = nave.color;
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -26,6 +47,7 @@ function drawBall() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayer()
     drawBall();
     x += dx;
     y += dy;
@@ -37,9 +59,6 @@ function draw() {
         dy = -dy;
     }
 
-
-
 }
-
 
 setInterval(draw, 10);
