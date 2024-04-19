868d771f719ef04adcc45a9a703dfa1605d59e88
Claudio Espinoza <claudioyugen@gmail.com>
2024-04-19 16:43:25 -0400

    Fix: Correccion sobre el objeto nave

--- script/Objetos/Nave.js
+++ script/Objetos/Nave.js
@@ -5,39 +5,13 @@ export class Nave {
     largo;
     posicion_X;
     posicion_Y;
-
-
+    
     constructor(posicion_X, posicion_Y, color, nombre, ancho, largo) {
-        this._posicion_X = posicion_X;
-        this._posicion_Y = posicion_Y;
-        this._color = color;
-        this._nombre = nombre;
-        this._ancho = ancho;
-        this._largo = largo;
-    }
-
-
-    get posicion_X() {
-        return this._posicion_X;
-    }
-
-    get posicion_Y() {
-        return this._posicion_Y;
-    }
-
-    get color() {
-        return this._color;
-    }
-
-    get nombre() {
-        return this._nombre;
-    }
-
-    get ancho() {
-        return this._ancho;
-    }
-
-    get largo() {
-        return this._largo;
+        this.posicion_X = posicion_X;
+        this.posicion_Y = posicion_Y;
+        this.color = color;
+        this.nombre = nombre;
+        this.ancho = ancho;
+        this.largo = largo;
     }
 }
