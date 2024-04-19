e1cca440afbd63991dbf4125540aa1ff08474e88
Esteban Candia <E.candxa@gmail.com>
2024-04-19 16:22:41 -0400

    pruebas js juego

--- script/script_game.js
+++ script/script_game.js
@@ -1,5 +1,45 @@
+import {Nave} from "./Objetos/Nave";
+
 
 // Referencia inicial al canvas y su respectivo contexto 2d
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+const nave = Nave(0,0,'green',"Nave 1",10,10)
+
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+var dx = 2;
+var dy = -2;
+
+var ballRadius = 10;
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+
+
+}
+
+
+setInterval(draw, 10);
