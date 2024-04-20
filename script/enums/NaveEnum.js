b01ae4febf56e7fa7fab4fb141359451df45102b
Claudio Espinoza <claudioyugen@gmail.com>
2024-04-20 19:28:59 -0400

    Fix: Modificar datos estaticos

--- script/enums/NaveEnum.js
+++ script/enums/NaveEnum.js
@@ -2,13 +2,13 @@
 export class NaveEnum {
 
     static POSICION_INICIAL_X_NAV_1 = 120;
-    static POSICION_INICIAL_X_NAV_2 = 880;
-    static POSICION_INICIAL_Y_NAV = 300;
+    static POSICION_INICIAL_X_NAV_2 = 850;
+    static POSICION_INICIAL_Y_NAV = 280;
 
     static COLOR_NAV_1 = "blue";
     static COLOR_NAV_2 = "red";
 
-    static ANCHO_NAV = 20;
+    static ALTO_NAV = 20;
     static LARGO_NAV = 30;
 
     static NOMBRE_NAV_1 = "Nave 1"
