291ad3afa9a30cff6a6e4ae18e5906ff6d2a5fdd
Claudio Espinoza <claudioyugen@gmail.com>
2024-04-20 21:49:29 -0400

    Refactor: Agregar vida a NavEnum

--- script/enums/NaveEnum.js
+++ script/enums/NaveEnum.js
@@ -1,16 +1,24 @@
 
 export class NaveEnum {
 
+    //Atributos de posición
     static POSICION_INICIAL_X_NAV_1 = 120;
     static POSICION_INICIAL_X_NAV_2 = 850;
     static POSICION_INICIAL_Y_NAV = 280;
 
+    //Atributos de vida
+    static VIDA_NAV_1 = 5;
+    static VIDA_NAV_2 = 5;
+
+    //Atributos de color
     static COLOR_NAV_1 = "blue";
     static COLOR_NAV_2 = "red";
 
+    //Atributos de tamaño
     static ALTO_NAV = 20;
     static LARGO_NAV = 30;
 
+    //Atributos de nombre
     static NOMBRE_NAV_1 = "Nave izquierda"
     static NOMBRE_NAV_2 = "Nave derecha"
 
