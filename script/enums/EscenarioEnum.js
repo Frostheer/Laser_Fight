fbba24d12e47d3d0b99b94e79198f0df5a83a935
Esteban Candia <E.candxa@gmail.com>
2024-04-20 23:53:24 -0400

    Update EscenarioEnum
    Se agregaron variables para definir limites del mapa

--- script/enums/EscenarioEnum.js
+++ script/enums/EscenarioEnum.js
@@ -5,4 +5,9 @@ export class EscenarioEnum{
 
     static TIEMPO_ESPERA = 500;
 
+    //Define los limites del mapa en distintas dimensiones (X e Y)
+    static LIMITE_MAPA_Y = 600;
+    static LIMITE_MAPA_X = 1000;
+    static LIMITE_MAPA_MITAD_X = 500;
+
 }
